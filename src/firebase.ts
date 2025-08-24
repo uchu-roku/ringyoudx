fc8fe499892bedfad5e1c130f79e603c94fd67d8
mt <sukrtitirou@outlook.jp>
2025-08-24 13:00:34 +0900

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFCXGOsvnc4-TN88J5ioV8H3MC0kvMiXg",
+  authDomain: "ringyoudx.firebaseapp.com",
+  projectId: "ringyoudx",
+  storageBucket: "ringyoudx.firebasestorage.app",
+  messagingSenderId: "529402072238",
+  appId: "1:529402072238:web:c47cb5e83dd39175a5b13f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
